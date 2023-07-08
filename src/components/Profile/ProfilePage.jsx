da5a721265776d3a9ade95d001e6e13d5eb827ce
Mark Veskov <mark.veskov@abv.bg>
2023-07-08 16:38:19 +0300

    Added personal info page with input validation

--- src/components/Profile/ProfilePage.jsx
+++ src/components/Profile/ProfilePage.jsx
@@ -12,6 +12,11 @@ import PersonSearchIcon from '@mui/icons-material/PersonSearch';
 import SettingsIcon from '@mui/icons-material/Settings';
 import StyleIcon from '@mui/icons-material/Style';
 import LogoutIcon from '@mui/icons-material/Logout';
+import EditIcon from '@mui/icons-material/Edit';
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import { IconButton } from '@mui/material';
 import AppBarNotMainPage from '../shared/HeaderNotMainPage/AppBarNotMainPage';
 import { viravaDefault } from '../../config/authConfig';
 
@@ -71,6 +76,45 @@ const ProfilePage = () => {
     reader.readAsDataURL(e.target.files[0]);
   };
 
+  const [email, setEmail] = useState('veskovmark99@gmail.com');
+  const [password, setPassword] = useState('********');
+  const [phoneNumber, setPhoneNumber] = useState('1234567890');
+  const [country, setCountry] = useState('Bulgaria');
+  const [city, setCity] = useState('Pernik');
+  const [isEditing, setIsEditing] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const isEmailValid = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const isPasswordValid = () => {
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    return passwordRegex.test(password);
+  };
+
+  const handleSaveClick = () => {
+    if (isEmailValid() && isPasswordValid()) {
+      setIsEditing(false);
+      setEmailError(false);
+      setPasswordError(false);
+    } else {
+      setEmailError(!isEmailValid());
+      setPasswordError(!isPasswordValid());
+    }
+  };
+
+  const handlePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   const renderMainSection = () => {
     switch (activeSection) {
       case 'accounts':
@@ -78,7 +122,73 @@ const ProfilePage = () => {
       case 'cards':
         return <div>Cards Section</div>;
       case 'personal':
-        return <div>Personal Information Section</div>;
+        return (
+          <div className="formContainer">
+            <div className="inputContainer">
+              <TextField
+                label="Email"
+                value={email}
+                disabled={!isEditing}
+                onChange={(e) => setEmail(e.target.value)}
+                error={emailError}
+                helperText={emailError ? 'Invalid email address' : ''}
+              />
+            </div>
+            <div className="inputContainer">
+              <TextField
+                label="Password"
+                value={password}
+                type={showPassword ? 'text' : 'password'}
+                error={passwordError}
+                disabled={!isEditing}
+                onChange={(e) => setPassword(e.target.value)}
+                InputProps={{
+                  endAdornment: (
+                    <IconButton onClick={handlePasswordVisibility}>
+                      {showPassword ? (
+                        <VisibilityIcon />
+                      ) : (
+                        <VisibilityOffIcon />
+                      )}
+                    </IconButton>
+                  )
+                }}
+              />
+            </div>
+            {passwordError && (
+              <p className="passwordRequirements">
+                Password must be at least 8 characters long and contain an
+                uppercase letter and a number.
+              </p>
+            )}
+            <TextField
+              label="Phone Number"
+              value={phoneNumber}
+              disabled={!isEditing}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+            <TextField
+              label="Country"
+              value={country}
+              disabled={!isEditing}
+              onChange={(e) => setCountry(e.target.value)}
+            />
+            <TextField
+              label="City"
+              value={city}
+              disabled={!isEditing}
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <div className="iconMainDiv">
+              <IconButton onClick={handleEditClick} className="iconSubDiv">
+                <EditIcon />
+              </IconButton>
+              <IconButton onClick={handleSaveClick} className="iconDiv">
+                <DoneAllIcon />
+              </IconButton>
+            </div>
+          </div>
+        );
       case 'settings':
         return <div>Settings Section</div>;
       default:
--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -9,62 +9,73 @@ import bgLogo from '../../../assets/bgLogo.png';
 
 const Footer = () => {
   return (
-    <><footer>
-      <div className="textFooter">
-        <div className="col1">
-          <h6>Main</h6>
-          <a>Transactions</a>
-          <a>Exchange</a>
-          <a>Subscriptions</a>
-          <a>Cards</a>
-        </div>
-        <div className="col2">
-          <h6>Subscription Plans</h6>
-          <a>Standard</a>
-          <a>Premium</a>
-          <a>Elite</a>
-        </div>
-        <div className="col3">
-          <h6>Organization</h6>
-          <a>About Us</a>
-          <a>Contacts</a>
-        </div>
-        <div className="col4">
-          <h6>Policies</h6>
-          <a>Terms & Conditions</a>
-          <a>Rights</a>
-        </div>
-      </div>
-      <div className="socialFooter">
-        <div className="footerLogos">
-          <a href="#" className="anchorAli">
-            <img src={whiteLogo} className="aliLogoWhite" />
-          </a>
-          <div className="socialLogosBlock">
-            <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
-              <img src={fbLogo} className="socialLogos" />
-            </a>
-            <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
-              <img src={twLogo} className="socialLogos" />
-            </a>
-            <a
-              href="https://www.instagram.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={instaLogo} className="socialLogos" />
-            </a>
+    <>
+      <footer>
+        <div className="textFooter">
+          <div className="col1">
+            <h6>Main</h6>
+            <a>Transactions</a>
+            <a>Exchange</a>
+            <a>Subscriptions</a>
+            <a>Cards</a>
+          </div>
+          <div className="col2">
+            <h6>Subscription Plans</h6>
+            <a>Standard</a>
+            <a>Premium</a>
+            <a>Elite</a>
+          </div>
+          <div className="col3">
+            <h6>Organization</h6>
+            <a>About Us</a>
+            <a>Contacts</a>
+          </div>
+          <div className="col4">
+            <h6>Policies</h6>
+            <a>Terms & Conditions</a>
+            <a>Rights</a>
           </div>
         </div>
-        <div className="line" />
-        <div className="bgFooter">
-          <img src={bgLogo} />
-          <span>Bulgaria</span>
+        <div className="socialFooter">
+          <div className="footerLogos">
+            <a href="#" className="anchorAli">
+              <img src={whiteLogo} className="aliLogoWhite" />
+            </a>
+            <div className="socialLogosBlock">
+              <a
+                href="https://www.facebook.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={fbLogo} className="socialLogos" />
+              </a>
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={twLogo} className="socialLogos" />
+              </a>
+              <a
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={instaLogo} className="socialLogos" />
+              </a>
+            </div>
+          </div>
+          <div className="line" />
+          <div className="bgFooter">
+            <img src={bgLogo} />
+            <span>Bulgaria</span>
+          </div>
         </div>
-      </div>
-    </footer><div className="copyright">
+      </footer>
+      <div className="copyright">
         Copyright © 2023 AliVolut - All rights reserved
-      </div></>
+      </div>
+    </>
   );
 };
 
